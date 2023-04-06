814fb223be52811e4915628fdfbf5b72571a8665
tobiasjohansson89 <35295187+tobiasjohansson89@users.noreply.github.com>
2023-04-06 14:54:09 +0200

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return (
+    <>
+    <nav>
+        <ul>
+            <li>
+                <div className="navbar">
+                    <span>Welcome</span>
+                </div>          
+            </li>
+        </ul>
+    </nav>
+    </>
+    );
+    }
